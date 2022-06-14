ca36ce541b90391f11138d519f0ce0e914791ade
Marty <martynolan17@gmail.com>
2022-06-13 22:34:47 -0400

    added number of characters/confirm prompts

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ var upperCase = ["abcdefghijklmnopqrstuv"];
 var lowerCase = ["ABCDEFGHIJKLMNOPQRSTUV"]
 var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
 
+function generatePassword () {
+  var numberOfChar  = prompt("How many characters do you want in your password?");
+
+  if (numberOfChar < 8 || numberOfChar >= 128) {
+    prompt("Choose a correct amount of characters!"); 
+  } else {
+    prompt(`Your password will have ${numberOfChar} characters.`);
+  }
+
+  var specialChar = confirm("Do you want your password to have special characters?");
+  var numbers = confirm("Do you want your password to numbers?");
+  var upperCase = confirm("Do you want uppercase characters in your password?");
+  var lowerCase = confirm("Do you want lowercase characters in your password?");
+
+}
+  
+  
+  
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
