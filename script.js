7b9ba4436edefc973eccf0f84ba72739f954d3eb
Marty <martynolan17@gmail.com>
2022-06-14 23:05:05 -0400

    added if statements to let user know what will be in their password

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Assignment code here
 
-var numbers = ["123456789"];
-var upperCase = ["abcdefghijklmnopqrstuv"];
-var lowerCase = ["ABCDEFGHIJKLMNOPQRSTUV"]
-var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~"]
+
+
+var diffCharacters = [];
+numberOfChar = "";
 
 function generatePassword () {
   var numberOfChar  = prompt("How many characters do you want in your password?");
@@ -11,16 +15,31 @@ function generatePassword () {
   if (numberOfChar < 8 || numberOfChar >= 128) {
     prompt("Choose a correct amount of characters!"); 
   } else {
-    prompt(`Your password will have ${numberOfChar} characters.`);
+    confirm(`Your password will have ${numberOfChar} characters.`);
   }
 
-  var specialChar = confirm("Do you want your password to have special characters?");
-  var numbers = confirm("Do you want your password to numbers?");
-  var upperCase = confirm("Do you want uppercase characters in your password?");
-  var lowerCase = confirm("Do you want lowercase characters in your password?");
+
+
+  var confirmSpecial = confirm("Do you want your password to have special characters?");
+  if (confirmSpecial) {
+    confirm("Your password will have special characters.")
+  } 
+  var confirmNumbers = confirm("Do you want your password to numbers?");
+  if (confirmNumbers) {
+    confirm("Your password will have numbers in it")
+  }
+  var confirmUpperCase = confirm("Do you want uppercase characters in your password?");
+  if (confirmUpperCase) {
+    confirm("Your password will have uppercase letters in it.")
+  }
+  var confirmLowerCase = confirm("Do you want lowercase characters in your password?");
+  if (confirmLowerCase) {
+    confirm("Your password will have lowercase letters in it.")
+  }
+
 
 }
-  
+
   
   
 
